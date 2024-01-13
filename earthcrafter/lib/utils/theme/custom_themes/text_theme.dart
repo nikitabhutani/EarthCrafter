d44c1ae06747db0000192f571ebc7a7c1d071083
nikitabhutani <nikitabhutani024@gmail.com>
2024-01-13 14:19:00 +0530

    second

---
+++ earthcrafter/lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ETextTheme {
+  ETextTheme._();
+  static TextTheme lighttexttheme = TextTheme();
+  static TextTheme darktexttheme = TextTheme();
+}
---
+++ earthcrafter/lib/utils/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:earthcrafter/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class EApptheme {
+  EApptheme._();
+  static ThemeData Lighttheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Montserrat',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: ETextTheme.lighttexttheme,
+  );
+  static ThemeData Darktheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Montserrat',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: ETextTheme.darktexttheme,
+  );
+
+}
